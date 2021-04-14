10fd9b22a98cb98318927915d05ce269a6f880fa
Roman Dubovik <ip.romandubovik@gmail.com>
2021-04-14 15:12:53 +0300

    all functionality added. project finished.

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
-document.querySelector('form').addEventListener('submit', (e) => {
+const { hash } = window.location;
+
+const message = atob(hash.replace('#', ''));
+
+if (message) {
+  // hide message form
+  document.querySelector('#message-form').classList.add('hide');
+  // show message panel
+  document.querySelector('#message-show').classList.remove('hide');
+  // add message to DOM
+  document.querySelector('#message-show h1').textContent = message;
+}
+console.log(atob(hash.replace('#', '')));
+
+document.querySelector('form').addEventListener('submit', function (e) {
   e.preventDefault();
 
-  const input = document.querySelector('input');
+  // show card with sharable link
+  document.querySelector('#link-form').classList.remove('hide');
+  // hide message form
+  document.querySelector('#message-form').classList.add('hide');
 
+  const input = document.querySelector('#message-input');
+  // create encrypted message
+  const encrypted = btoa(input.value);
+
+  const linkInput = document.querySelector('#link-input');
+  // create link to share with encrypted message
+  linkInput.value = `${window.location}#${encrypted}`;
+  // pre-select created link
+  linkInput.select();
 });
